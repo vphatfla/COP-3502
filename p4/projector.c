77f65b125c9c4f48ad9ddcb131e7203ea8be631f
van wsl2 <pvphat25@gmail.com>
2023-10-23 21:00:28 -0400

    p4 started

--- p4/projector.c
+++ p4/projector.c
@@ -32,37 +32,43 @@ double degreesToRadians(double degrees);
 // merge sort
 void Merge(group** list_group, int start, int mid, int end);
 void MergeSort(group** lsit_group, int start, int end);
+// calculate minimum number of peoplein rA
+void minimumPeopleInRange(group** list_group);
 int main() {
     // take input
     scanf("%d %d", &N, &A);
     rA = degreesToRadians(A);
 
     // allocate memory for list of group
-    group **list_group = malloc(N* sizeof(group*));
+    group **list_group = malloc((N*2)* sizeof(group*));
     
     // get input for N groups
     for (int i=0; i<N; i+=1) {
         int x,y,s;
         scanf("%d %d %d", &x,&y,&s);
         list_group[i] = createNewGroup(x,y,s);
+        list_group[N+i] = createNewGroup(x,y,s);
+        list_group[N+i]->angle = (list_group[N+i]->angle + (2*M_PI));  
     }
 
     // print out group information
-    for (int i=0; i<N; i+=1) {
+    for (int i=0; i<N*2; i+=1) {
         printf("Group %d\n", i);
         printf("x = %d, y = %d, s = %d \n",list_group[i]->x, list_group[i]->y, list_group[i]->s);
         printf("Angle = %f\n", list_group[i]->angle);
     }
 
     // sort the list_group based on the angle
-    MergeSort(list_group, 0, N-1);
-
+    MergeSort(list_group, 0, N*2-1);
+    printf("AFTER SORT -----------------------------\n\n\n");
     // reprint
-    for (int i=0; i<N; i+=1) {
+    for (int i=0; i<N*2; i+=1) {
         printf("Group %d\n", i);
         printf("x = %d, y = %d, s = %d \n",list_group[i]->x, list_group[i]->y, list_group[i]->s);
         printf("Angle = %f\n", list_group[i]->angle);
     }
+
+
     freeMemory(list_group);
     return 0;
 }
@@ -75,7 +81,9 @@ group* createNewGroup(int x, int y, int s) {
     newGroup->angle = calculateAngle(x,y);
     return newGroup;
 }
-
+void minimumPeopleInRange(group** list_group) {
+    int count =0;
+}
 double calculateAngle(int x, int y) {
     return atan2(y,x);
 }
@@ -89,7 +97,7 @@ double degreesToRadians(double degrees) {
 }
 
 void freeMemory(group **list_group) {
-    for (int i=0; i<N; i+=1) {
+    for (int i=0; i<N*2; i+=1) {
         free(list_group[i]);
     }
     free(list_group);
@@ -109,6 +117,8 @@ void Merge(group** list_group, int start, int mid, int end) {
     newIndex = 0;
     // Here we copy values into our auxiliary array, so long as there are 
     // numbers from both lists to copy.
+    left = start;
+    right = mid;
     while ((left < mid) || (right <= end)) { 
         temp[newIndex] = malloc(sizeof(group));
         if (right>end || (left < mid && list_group[left]->angle < list_group[right]->angle)) {
