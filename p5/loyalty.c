ca424f68d8e68ba74423d72b65561874dfcce535
van-wsl-ubuntu <pvphat25@gmail.com>
2023-11-03 21:46:20 -0400

    work on the size next

--- p5/loyalty.c
+++ p5/loyalty.c
@@ -3,7 +3,303 @@ Van Phat Phan
 COP-3502 Section 4
 11/03/2023*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 19
+
+// global variables, N = number of commands, num_customer = total number of customers;
+int N, num_customer;
+
+typedef struct customer {
+ char name[MAXLEN+1];
+ int points;
+} customer;
+
+typedef struct treenode {
+ customer* cPtr;
+ int size;  // store total number of nodes in the subtree rooted at that node, including itself
+ struct treenode* left;
+ struct treenode* right;
+} treenode;
+
+// new customer
+customer* create_new_customer(char name[], int points);
+
+// node implementation
+treenode* create_new_node(customer* customer);
+
+// Traversals.
+void preorder(struct treenode *current_ptr);
+void inorder(struct treenode *current_ptr);
+
+// Search and insert.
+treenode* findNode(struct treenode *current_ptr, char name[]);
+treenode* insert(treenode *root, treenode *element);
+
+// Structural functions to aid delete.
+int isLeaf(struct treenode *node);
+int hasOnlyLeftChild(struct treenode *node);
+int hasOnlyRightChild(struct treenode *node);
+
+
+// Other functions to aid delete.
+struct treenode* parent(struct treenode *root, struct treenode *node);
+struct treenode* minVal(struct treenode *root);
+struct treenode* maxVal(struct treenode *root);
+
+// Delete functions.
+struct treenode* delete(struct treenode* root, char name[]);
+
+// ultilities functions
+int compare_string(char name1[], char name2[]);
+void free_memory_node(treenode *node);
+
+// command functions 
+void add_command();
+void sub_command();
+void del_command();
+void count_smaller_command();
 
 int main() {
+    // init num_customer
+    num_customer = 0;
+    scanf("%d", N);
+    for (int i = 0; i< N; i+=1) {
+        char command[15]; // command max length is 15, count_smaller isn't even 15, safety upper bound
+        scanf("%s", command);
+
+    }
     return 0;
+}
+
+customer* create_new_customer(char name[], int points) {
+    customer* new_customer = malloc(sizeof(customer));
+    strcpy(new_customer->name, name);
+    new_customer->points = points;
+    return new_customer;
+}
+
+treenode* create_new_node(customer* customer) {
+    treenode* new_treenode = malloc(sizeof(treenode));
+    new_treenode->cPtr = customer;
+    new_treenode->size = 0;
+    new_treenode->left = NULL;
+    new_treenode->right = NULL;
+    return new_treenode;
+}
+
+int compare_string(char name1[], char name2[]) {
+    return strcmp(name1, name2);
+}
+
+void free_memory_node(treenode *node) {
+    free(node->cPtr);
+    free(node);
+}
+// Inserts a the node pointed to by element in the tree rooted by
+// root and returns a pointer to the resulting tree.
+struct treenode* insert(treenode* root,
+                         treenode* element) {
+
+    // Inserting into an empty tree.
+    if (root == NULL)
+        return element;
+
+    // Regular case.
+    else {
+        int compare_result = compare_string(element->cPtr->name, root->cPtr->name);
+        
+        // customer existed, update the points
+        if (compare_result == 0) {
+            root->cPtr->points += element->cPtr->points;
+            free_memory_node(element);
+        }
+        // element name is greater than root
+        // element should be inserted to the right.
+        else if (compare_result > 0) {
+
+            // There is a right subtree to insert the node.
+            if (root->right != NULL)
+                root->right = insert(root->right, element);
+
+            // Place the node directly to the right of root.
+            else
+                root->right = element;
+        }
+
+        // element should be inserted to the left.
+        else {
+
+            // There is a left subtree to insert the node.
+            if (root->left != NULL)
+                root->left = insert(root->left, element);
+
+            // Place the node directly to the left of root.
+            else
+                root->left = element;
+        }
+
+        // Return the root pointer of the updated tree.
+        return root;
+    }
+}
+
+// Returns a pointer to a node that stores value in it in the subtree
+// pointed to by current_ptr. NULL is returned if no such node is found.
+struct treenode* findNode(struct treenode *current_ptr, char name[]) {
+    int compare_result = compare_string(name, current_ptr->cPtr->name);
+    // Check if there are nodes in the tree.
+    if (current_ptr != NULL) {
+
+        // Found the value at the root.
+        if (compare_result)
+            return current_ptr;
+
+        // Search to the left.
+        if (compare_result < 0)
+            return findNode(current_ptr->left, name);
+
+        // Or...search to the right.
+        else
+            return findNode(current_ptr->right, name);
+    }
+
+    // Null case.
+    else
+        return NULL;
+}
+
+// Structural functions to aid delete.
+int isLeaf(struct treenode *node) {
+    return (node->left == NULL && node->right == NULL);
+}
+int hasOnlyLeftChild(struct treenode *node) {
+    return (node->left != NULL && node->right == NULL);
+}
+int hasOnlyRightChild(struct treenode *node) {
+    return (node->left == NULL && node->right != NULL);
+}
+
+// returns the parent of the node pointed to by node in the tree
+//rooted at root, if the node is the root of the tree, 
+// or node doesn't exist in the tree, null will be returned
+struct treenode* parent(struct treenode *root, struct treenode *node) {
+    
+    if (root ==NULL || root == node) 
+        return NULL;
+    
+    // the root is the direct parent of node
+    if (root->left == node || root->right == node)
+        return root;
+    
+
+    
+    int compare_result = compare_string(node->cPtr->name, root->cPtr->name);
+    // look for node's parent in the right side of the tree
+    if (compare_result > 0) 
+        return parent(root->right, node);
+    else if (compare_result < 0)
+        return parent(root->left, node);
+    
+    return NULL;
+}
+
+struct treenode* minVal(struct treenode *root) {
+    if (root->left == NULL)
+        return root;
+    
+    else return minVal(root->left);
+}
+struct treenode* maxVal(struct treenode *root)
+{
+    if (root->right == NULL)
+        return root;
+    
+    else return maxVal(root->right);
+}
+
+// Delete functions.
+struct treenode* delete(struct treenode* root, char name[]) {
+
+    treenode *delNode, *new_del_node, *save_node;
+    treenode *parentNode;
+    
+    delNode = findNode(root, name);
+    parentNode = parent(root, delNode);
+
+    if (isLeaf(delNode)) {
+        // delete the only node in the tree
+        if (parentNode == NULL) {
+            free_memory_node(root);
+            return NULL;
+        }
+
+        // delete if it is a left child
+        if (parentNode->left == delNode) {
+            free_memory_node(delNode);
+            parentNode->left = NULL;
+        }
+        else {
+            free_memory_node(delNode);
+            parentNode->right = NULL;
+        }
+
+        return root;
+    }
+
+    // only has left
+    if (hasOnlyLeftChild(delNode)) {
+
+        if (parentNode == NULL) {  // delnode is the root
+            save_node = delNode->left;
+            free_memory_node(delNode);
+            return save_node;  // new root
+        }
+
+        // delete a node if it's a left child
+        if (delNode == parentNode->left) {
+            save_node = parentNode->left;
+            parentNode->left = parentNode->left->left; // remove the left from the BST
+            free_memory_node(delNode);
+        }
+        else { // delete node if it's a right child
+            save_node = parentNode->right;
+            parentNode->right = parentNode->right->right; // remove the right from the BST
+            free_memory_node(delNode);
+        }
+        return root;    
+    }
+
+    // only has right child
+    if (hasOnlyRightChild(delNode)) {
+        if (parentNode == NULL) { // delnode is a root
+            save_node = delNode->right;
+            free_memory_node(delNode);
+            return save_node;
+        }
+        
+        // if delnode is a left child of parentnode
+        if (parentNode->left == delNode) {
+            parentNode->left = parentNode->left->right;
+            free_memory_node(delNode);
+        }
+        else { // delnot is a right child of parentnode
+            parentNode->right = parentNode->right->right;
+            free_memory_node(delNode);
+        }
+        return root;
+    }
+
+    // if none of these aboves are the case, then "switch the place of delnode customer with the maxVal"
+    // find the greatest on the left
+    new_del_node = maxVal(delNode->left);
+    customer* new_customer_pointer = new_del_node->cPtr;
+    
+    delete(root, new_del_node->cPtr->name); // recursively delete the node with the given name, kinda sketchy at this point
+
+    // restore the save data
+    delNode->cPtr = new_customer_pointer;
+
+    return root;
 }
